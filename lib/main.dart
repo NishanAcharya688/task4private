aa804f1050d4c3e278616d3909f8878fa1c03f5b
Nishan Acharya <nishana688@gmail.com>
2025-01-07 14:18:07 +0545

    test notifications from console

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:task4/api/firebase_api.dart';
 import 'package:task4/firebase_options.dart';
 import 'package:task4/views/home_page.dart';
+import 'package:task4/views/notification_page.dart';
 
+final navigatorKey = GlobalKey<NavigatorState>();
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseApi().initNotifications();
   runApp(MyApp());
 }
 
@@ -15,6 +19,10 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
+      navigatorKey: navigatorKey,
+      routes: {
+        '/notification_screen':(context)=> const NotificationPage(),
+      },
     );
   }
 }
